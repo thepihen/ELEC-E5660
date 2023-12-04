e6d6c87654a37184672f880dc3d9da66b2a6cef5
thepihen <96073767+thepihen@users.noreply.github.com>
2023-12-04 17:28:27 +0200

    Add files via upload

--- midi_main.m
+++ midi_main.m
@@ -28,7 +28,7 @@ userIn = "d";
 while(userIn ~= "y" && userIn ~= "n")
     userIn = input("Do you wish to play from your external MIDI keyboard? (y/n)","s");
 end
-playFromMIDI = (userIn == "y");
+playFromMIDI = (userIn == "y" || userIn == "Y");
 mididevinfo;
 selectedDevice = "KOMPLETE KONTROL M32 MIDI";
 
@@ -43,49 +43,150 @@ deviceWriter = audioDeviceWriter('SampleRate',44100, ...
     "Driver", "ASIO", "Device", "FL Studio ASIO","SupportVariableSizeInput", true,...
     "BitDepth", "24-bit integer");
 % asiosettings(deviceWriter.Device);
-deviceWriter.BufferSize = 512;
+deviceWriter.BufferSize = 256;
 if playFromMIDI
-    simplesynth(selectedDevice, deviceWriter, aa_lpc_coeffs, Fs);
+    simplesynthOptionalMIDI(deviceWriter, aa_lpc_coeffs, Fs, true, selectedDevice);
 else
-    simplesynthNoMIDI(deviceWriter, oo_lpc_coeffs, Fs);
+    simplesynthOptionalMIDI(deviceWriter, aa_lpc_coeffs, Fs, false, "none");
 end
 
-function simplesynthNoMIDI(deviceWriter,a_lpc, Fs)
+function simplesynthOptionalMIDI(deviceWriter,a_lpc, Fs, playingFromMidiFlag, selectedDevice)
+if(selectedDevice~="none")
+    midiInput = mididevice(selectedDevice);
+end
 deviceWriter.SupportVariableSizeInput = true;
 % a = a_lpc(:,40);
-a = a_lpc(:,1);
+% a = a_lpc(:,1);
 amp = 0.6;
-freq = 100;
-prevFreq = 0;
-remainder = zeros(deviceWriter.BufferSize,1);
+freq = 0;
+prevFreq = -1;
+% remainder = zeros(deviceWriter.BufferSize,1);
 N = deviceWriter.BufferSize;
-N = 512;
+% N = 512;
 h = deviceWriter.BufferSize/2;
-notes = [59,61,64,70,76,80,85];
-currNote = 1;
+notes = [59,61,63,70,76,80,85];
+currNote = 1; %for playing with a MIDI keyboard
+arrCurrNote = 1; %for playing without a MIDI keyboard
 delayBetweenNotes = 1;
 noteTic = tic;
-hasFinishedProcessingPreviousBlock = true;
+% hasFinishedProcessingPreviousBlock = true;
 currBlock = 1;
+%buildEnvelope(Fs, levels, times)
+%levels = [aLev, dLev, sLev]
+%times = [a-d, d-s, s, r]
+%the sustain one is not really needed, is for when you're playing without a
+%keyboard
+env_aLev = 0;
+env_dLev = 1;
+env_sLev = 0.2;
+env_ad = 1;
+env_ds = 0.5;
+env_s = 3;
+env_r = 0.2;
+
+[envelope,envTimes] = buildEnvelope(Fs, [env_aLev, env_dLev, env_sLev], [env_ad, env_ds, env_s, env_r]);
+envelope = envelope';
+% disp(size(envelope));
+ll = 4096;%block length
+env_AD = envelope(1:envTimes(1)+envTimes(2)); %this has to be applied automatically
+env_R = envelope(end-envTimes(3)+1:end);
+remainder_AD = mod(length(env_AD), ll);
+%envelope = [envelope, zeros(1, remainder)]';
+times = (length(env_AD)/ll) *(ll/N);
+envelope = env_AD;
+currTime = 1;
+
+isPlayingNote = false;
 while true
 
-    freq = note2freq(notes(currNote));
-    
-    if(toc(noteTic)>delayBetweenNotes)
-        currNote = currNote + 1;
-        if currNote > 7
-            currNote = 1;
+    if (playingFromMidiFlag)
+        msgs = midireceive(midiInput);
+
+        for i = 1:numel(msgs)
+            msg = msgs(i);
+            switch (msg.Type)
+                case midimsgtype.NoteOn
+                    freq = note2freq(msg.Note);
+                    amp = msg.Velocity/127;
+                    envelope = env_AD;
+                    times = (length(env_AD)/ll) *(ll/N);
+                    currNote = msg.Note;
+                    isPlayingNote = true;
+
+                    %buildEnvelope(Fs, levels, times)
+                    %levels = [aLev, dLev, sLev]
+                    %times = [a-d, d-s, s, r]
+                    %the sustain one is not really needed, is for when you're playing without a
+                    %keyboard
+                    [envelope,envTimes] = buildEnvelope(Fs, [env_aLev, ...
+                        env_dLev, env_sLev], [env_ad, env_ds, env_s, env_r]);
+                    envelope = envelope';
+                    % disp(size(envelope));
+                    ll = 4096;%block length
+                    env_AD = envelope(1:envTimes(1)+envTimes(2)); %this has to be applied automatically
+                    env_R = envelope(end-envTimes(3)+1:end);
+                    remainder_AD = mod(length(env_AD), ll);
+                    %envelope = [envelope, zeros(1, remainder)]';
+                    times = (length(env_AD)/ll) *(ll/N);
+                    envelope = env_AD;
+
+                    currTime = 1;
+
+                case midimsgtype.NoteOff
+                    % if msg.Note == msg.Note
+                    if(currNote == msg.Note)
+                        envelope = env_R;
+                        times = (length(envelope)/ll) *(ll/N);
+                        isPlayingNote = false;
+                        % end
+                        currTime = 1;
+                    end
+                case midimsgtype.ControlChange
+                    switch msg.CCNumber
+                        case 18
+                            %let's set a maximum of 5 seconds, and a
+                            %minimum of 10 milliseconds
+                            env_ad = (msg.CCValue/127 * 0.49) +0.01;
+                            disp("attack changed")
+                        case 19
+                            disp("decay chaged")
+                            %same here
+                            env_ds = (msg.CCValue/127 * 4.99) +0.01;
+                        case 20
+                            %this only changes the sustain level
+                            env_sLev = (msg.CCValue/127);%from 0 to 1
+                            disp("sustain changed")
+                        case 21
+                            disp("release changed")
+                            env_r= (msg.CCValue/127 * 3) ;
+                    end
+            end
         end
-        noteTic = tic;
+
+    else
+        currNote = notes(arrCurrNote);
+        freq = note2freq(notes(arrCurrNote));
+        if(toc(noteTic)>delayBetweenNotes)
+            arrCurrNote = arrCurrNote + 1;
+            if arrCurrNote > 7
+                arrCurrNote = 1;
+            end
+            noteTic = tic;
+        end
     end
-    
+
+
+
     if(freq~=prevFreq)
-        ll = 8192;
-        input = zeros(ll,1);
+        currTime = 1;
+        lpcInd = matchClosestLPCCoeff(currNote);
+        a = a_lpc(:,lpcInd);
+        % input = zeros(ll,1);
         n = 0:ll-1;
         n = n';
-        w0T = 2*pi*freq/Fs;
-        pitch = (Fs/freq);
+        % w0T = 2*pi*freq/Fs;
+        blockFreq = freq + randn(1);
+        pitch = (Fs/blockFreq);
         P = pitch;
         M = 2*floor(P/2) + 1;
         input = (M/P)*sin(pi* M*n/P)./(M*sin(pi*n/P));
@@ -98,8 +199,9 @@ while true
 
         block = filter(1,[1; -a], input);
         block = block/max(abs(block)) * amp;
-        
+
         prevFreq = freq;
+
         currBlock = 1;
     end
     sblock = block(1:N,:);
@@ -107,24 +209,63 @@ while true
         sblock2 = sblock.*hamming(length(sblock), 'periodic');
         sblock(1:length(sblock)/2, :) = sblock2(1:length(sblock2)/2, :);
     end
+    %there is a discontinuity between the beginning and the end (actually
+    %between the end and the beinning). A better algorithm would loop an
+    %area in the middle, but right now I don't have the time to follow
+    %that approach, hence this tries to address said discontinuity
+    %with some overlap-and-add between the last frame and the first one.
+    %This means one frame is "skipped"
     if(mod(currBlock, ll/N)==0)
         block = circshift(block, -length(sblock));
         nextBlock = block(1:N,:);
         fakeBlock = [nextBlock; sblock].*hamming(size(sblock,1)*2);
         sblock = fakeBlock(length(fakeBlock)/2 + 1:end) + fakeBlock(1:length(fakeBlock)/2);
-        currBlock = currBlock + 1; %you're skipping one
+        currBlock = currBlock + 1; %we're "skipping" one, we need to account for that
+        %or the algorithm will only work on the first round
     end
     % if(currBlock = ll/N)
     %     sblock2 = sblock.*hamming(length(sblock), 'periodic');
     %     sblock(1:length(sblock)/2, :) = sblock2(1:length(sblock2)/2, :);
     % end
+    disp(currTime+ " / " + times);
+    if(currTime <= times )
+        if(currTime == times )
+            %remainder
+            coveredEnv = [envelope(end-remainder+1:end); ones(length(envelope)-remainder, 1)*env_sLev*isPlayingNote];
+        else
+            coveredEnv = envelope((currTime-1)*N +1:currTime*N);
+        end
+    else
+        %the envelope will be incomplete
+        coveredEnv = ones(size(coveredEnv))*env_sLev*isPlayingNote;
+    end
+    disp(coveredEnv(10));
+    disp(env_s)
+    sblock = sblock.*coveredEnv*amp;
+    currTime = currTime +1;
+
+
     block = circshift(block, -length(sblock));
     deviceWriter([sblock, sblock]);
     currBlock = currBlock + 1;
     % block = circshift(block, -N);
+    % disp(currTime + " / "+times);
+
+
 end
 end
 
+
+function block = blockTransition(prevBlock, currBlock)
+windowed = currBlock .* hanning(length(currBlock));
+windowed_prev = prevBlock .* hanning(length(prevBlock));
+block = [windowed(1:length(currBlock)/2)+windowed_prev(length(windowed_prev)/2 + 1:end); currBlock(length(currBlock)/2+1:end)];
+end
+
+function c = matchClosestLPCCoeff(midiNoteNumber)
+arr = [60,64,67,72,76,79,84];
+[~,c] = min(abs(arr-midiNoteNumber));
+end
 
 function yes = isNoteOn(msg)
 yes = msg.Type == midimsgtype.NoteOn ...
@@ -151,6 +292,19 @@ y = sin(pi * x) ./ (pi * x);
 y(x == 0) = 1;
 end
 
+function [env,lengths] = buildEnvelope(Fs, levels, times)
+%levels = [aLev, dLev, sLev] %the sustain level is implied to be the
+%same as the release level
+%times = [a-d, d-s, s, r]
+%a,d,s,r = attack, decay , sustain, release in seconds
+aEnv = linspace(levels(1),levels(2), round(Fs*times(1)));
+dEnv = linspace(levels(2), levels(3), round(Fs*times(2)));
+sEnv = linspace(levels(3), levels(3), round(Fs*times(3)));
+rEnv = linspace(levels(3), 0, round(Fs*times(4)));
+env = [aEnv, dEnv, sEnv, rEnv];
+lengths = [length(aEnv), length(dEnv), length(rEnv)];
+end
+
 
 
 %https://www.music.mcgill.ca/~gary/307/week5/node14.html
@@ -160,8 +314,8 @@ midiInput = mididevice(midiDeviceName);
 deviceWriter.SupportVariableSizeInput = true;
 a = a_lpc(:,40);
 amp = 0.6;
-freq = 100;
-prevFreq = 0;
+freq = 0;
+prevFreq = -1;
 remainder = zeros(deviceWriter.BufferSize,1);
 N = deviceWriter.BufferSize;
 h = deviceWriter.BufferSize/2;
@@ -248,3 +402,16 @@ while true
     block = circshift(block, -N);
 end
 end
+
+
+%% TODOS
+%-if you are moving from one note to the next (noteOff after a noteOn) then
+%the pitch should "slide" to the new one (using the blockTransition
+%function)
+%
+%-make the script work for any buffer size, always keeping 256-samples
+%sub-buffers you will use to build larger ones
+%
+%-move to midi
+%
+%-midi noteOn, noteOff detection, and maybe CC controls
